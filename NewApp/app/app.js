5d913231e5369659df0f129de34ff0b136086e99
Naman <bhargava.n@husky.neu.edu>
2018-06-21 22:47:22 -0400

    updating server

--- NewApp/app/app.js
+++ NewApp/app/app.js
@@ -11,7 +11,9 @@ app.use(require('./routes/chat'));
 app.use(require('./routes/feedback'));
 app.use(require('./routes/api'));
 
-var server = app.listen(3100);
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 io.attach(server);
 
